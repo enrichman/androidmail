20fda2e486abb63fd3c9cefc6139167a773695c9
Enrico Candino <enrico.candino@gmail.com>
2016-06-22 22:30:35 +0200

    first commit

---
+++ sample/src/main/java/it/enricocandino/mail/sample/MainActivity.java
@@ -0,0 +1,20 @@
+package it.enricocandino.mail.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import it.enricocandino.sample.R;
+
+/**
+ * Copyright (c) 2016 Enrico Candino
+ * <p>
+ * Distributed under the MIT License.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
